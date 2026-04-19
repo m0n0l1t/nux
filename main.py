01daa99f90de2c2ccba23abd029647a9414f007a
mon0l <mon0l1t@bk.ru>
2026-04-20 06:15:57 +0700

    добавил статику

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Указываем папку, где лежат наши HTML-файлы
 templates = Jinja2Templates(directory="templates")
+
+# Монтируем статические файлы (CSS, JS, изображения)
+app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
